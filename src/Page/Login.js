5a0db16c201f3ae5405aeffe43e0136fe980b586
junny1995 <while0818@gmail.com>
2023-02-09 16:15:11 +0900

    feat: 회원가입, 로그인기능만 추가

--- src/Page/Login.js
+++ src/Page/Login.js
@@ -1,8 +1,41 @@
+import axios from "axios";
+import { useState } from "react";
+
 function Login() {
+  // env URL
+  const URL = process.env.REACT_APP_API_URL;
+
+  // useState
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // 이메일, 패스워드 value
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const onChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const SignIpHandler = async () => {
+    try {
+      const a = await axios.post(`${URL}/auth/signin`, {
+        email: email,
+        password: password,
+      });
+      console.log(a);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
-    <>
-      <div>로그인페이지</div>
-    </>
+    <div>
+      <input onChange={onChangeEmail} placeholder="이메일" />
+      <input onChange={onChangePassword} placeholder="비밀번호" />
+      <button onClick={SignIpHandler}>로그인</button>
+    </div>
   );
 }
 
--- src/Page/Signup.js
+++ src/Page/Signup.js
@@ -1,8 +1,41 @@
+import axios from "axios";
+import { useState } from "react";
+
 function Signup() {
+  // env URL
+  const URL = process.env.REACT_APP_API_URL;
+
+  // useState
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // 이메일, 패스워드 value
+  const onChangeEmail = (e) => {
+    const emailCurrent = e.target.value;
+    setEmail(emailCurrent);
+  };
+
+  const onChangePassword = (e) => {
+    const passwordCurrent = e.target.value;
+    setPassword(passwordCurrent);
+  };
+
+  const SignUpHandler = async () => {
+    try {
+      await axios.post(`${URL}/auth/signup`, {
+        email: email,
+        password: password,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
-    <>
-      <div>회원가입페이지</div>
-    </>
+    <div>
+      <input onChange={onChangeEmail} placeholder="이메일" />
+      <input onChange={onChangePassword} placeholder="비밀번호" />
+      <button onClick={SignUpHandler}>회원가입</button>
+    </div>
   );
 }
 
